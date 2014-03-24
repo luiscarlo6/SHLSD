cb23688a968f573ec78e32a238e3ade73bb69e3c
John Delgado <johndelgado@MacBook-Pro-de-John.local>
2014-03-24 08:12:06 -0430

    Solucionados varios bugs, se agregaron verificaciones e imagenes

---
+++ NetbeansSHLSD/src/Graphic/Hora.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Graphic;
+
+import java.util.regex.Pattern;
+
+/**
+ *
+ * @author johndelgado
+ */
+class Hora {
+    String cadena;
+    Integer hora;
+    Integer minutos;
+
+    public Hora(String cadena, Integer hora, Integer minutos) {
+        this.cadena = cadena;
+        this.hora = hora;
+        this.minutos = minutos;
+    }
+    
+    public Hora(String cadena){
+        String literal1 = cadena.substring(0, 2);
+        String literal2 = cadena.substring(3, 5);
+        if (cadena.contains(":")){
+            this.cadena = cadena;
+            this.hora = Integer.parseInt(literal1);
+            this.minutos = Integer.parseInt(literal2);
+        }
+
+    }
+    
+    public Hora(Integer hora, Integer minutos) {
+        this.cadena = "Hora calculada";
+        this.hora = hora;
+        this.minutos = minutos;
+    }
+    
+    public static Hora diferencia(Hora inicio, Hora fin){
+        Integer totalHoras = 0;
+        Integer totalMinutos = 0;
+        
+        if (fin.minutos >= inicio.minutos ){
+            totalHoras = fin.hora - inicio.hora;
+            totalMinutos = fin.minutos - inicio.minutos;
+        }
+        
+        if (fin.minutos < inicio.minutos ){
+            if (fin.hora == inicio.hora + 1){
+                totalHoras = 0;
+            }else{
+                totalHoras = fin.hora - inicio.hora - 1;
+            }
+            totalMinutos = (60 - inicio.minutos) + fin.minutos;
+        }
+        
+        Hora resultado = new Hora(totalHoras,totalMinutos);
+        
+        return resultado;
+    }
+
+    public String getCadena() {
+        return cadena;
+    }
+
+    public void setCadena(String cadena) {
+        this.cadena = cadena;
+    }
+
+    public Integer getHora() {
+        return hora;
+    }
+
+    public void setHora(Integer hora) {
+        this.hora = hora;
+    }
+
+    public Integer getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(Integer minutos) {
+        this.minutos = minutos;
+    }
+
+    @Override
+    public String toString() {
+        return "Hora{" + "cadena=" + cadena + ", hora=" + hora + ", minutos=" + minutos + '}';
+    }
+    
+    public Double toDouble() {
+        Double resultado = this.hora + ((this.minutos) /100.0);
+        return resultado;
+    }
+    
+    public Integer esMayor(Hora inicio, Hora fin){        
+        return  inicio.hora < fin.hora ? 1 :0;
+    }
+    
+    public Boolean esValida(){
+        return (this.hora < 24 && this.hora > 0 && this.minutos < 60 && this.minutos > 0);
+    }
+    
+    public static Boolean validarFormato(String formato) {
+        String patron = "[0-9][0-9]:[0-9][0-9]";
+        Pattern comp = Pattern.compile(patron);
+        return formato.matches(patron);
+    }
+    
+    public static Boolean validarFormatoFecha(String formato) {
+        String patron = "[0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]";
+        Pattern comp = Pattern.compile(patron);
+        return formato.matches(patron);
+    }
+}
